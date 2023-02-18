d02680031844d14bb895bde3363eebca4d54a38e
Will <happyeungin@gmail.com>
2023-02-18 23:13:54 +0800

    update ai.py

---
+++ whatsapp/ai.py
@@ -0,0 +1,24 @@
+import os
+import openai
+from django.conf import settings
+openai.api_key = settings.OPENAI_API_KEY
+
+def startPrompt(instruction):
+
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="{}".format(instruction),
+        temperature=0.7,
+        max_tokens=300,
+        n=1
+    )
+
+    if 'choices' in response:
+        if len(response['choices'])>0:
+            answer = response['choices'][0]['text']
+            return answer
+        else:
+            return ''
+    
+    else:
+        return ''
