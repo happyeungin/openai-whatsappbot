51752f0227ca5fa7235f481023d9712189ff8172
Will <happyeungin@gmail.com>
2023-02-18 11:56:44 +0800

    4th change

--- whatsapp/functions.py
+++ whatsapp/functions.py
@@ -2,7 +2,7 @@ from django.conf import settings
 import requests
 
 def sendWhatsAppMessage(phoneNumber, message):
-    headers = {"Authoriztion": settings.WHATSAPP_TOKEN}
+    headers = {"Authorization": settings.WHATSAPP_TOKEN}
     payload = { "messaging_product": "whatsapp",
                 "recipient_type": "individual",
                 "to": phoneNumber,
@@ -12,8 +12,9 @@ def sendWhatsAppMessage(phoneNumber, message):
 
     response = requests.post(settings.WHATSAPP_URL, headers=headers, json=payload)
     ans = response.json()
+    return ans
 
-phoneNumber = "85269709752"
-message = "Hello There!!!!!!"
+# phoneNumber = "85269709752"
+# message = "Hello There!!!!!!"
 
-sendWhatsAppMessage(phoneNumber, message)
+# sendWhatsAppMessage(phoneNumber, message)
