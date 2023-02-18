a23a841bafa8c578fef0b9389153cffedbff8eb0
Will <happyeungin@gmail.com>
2023-02-18 19:21:32 +0800

    8th change

--- whatsapp/urls.py
+++ whatsapp/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    # path('123', views.whatsAppWebhook, name="whatsapp-webhook"),
+    path('510ed578-502a-43d3-9ed2-c69e7d2bdacc', views.whatsAppWebhook, name="whatsapp-webhook"),
 ]
+
+
+# UUID = 510ed578-502a-43d3-9ed2-c69e7d2bdacc
+# Token = 2dc84afb-4bc9-46e3-a750-049bf6cf0483
--- whatsapp/views.py
+++ whatsapp/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
 
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse, HttpResponse
+from .functions import *
+import json
+
+
 # Create your views here.
 def home(request):
     return render(request, 'whatsapp/index.html', {})
 
+
+
+@csrf_exempt
+def whatsAppWebhook(request):
+    if request.method == 'GET':
+        VERIFY_TOKEN = '2dc84afb-4bc9-46e3-a750-049bf6cf0483'
+        mode = request.GET['hub.mode']
+        token = request.GET['hub.verify_token']
+        challenge = request.GET['hub.challenge']
+
+        if mode == 'subscribe' and token == VERIFY_TOKEN:
+            return HttpResponse(challenge, status=200)
+        else:
+            return HttpResponse('error', status=403)
+
+
+    if request.method == 'POST':
+        data = json.loads(request.body)
+
+        return HttpResponse('success', status=200)
