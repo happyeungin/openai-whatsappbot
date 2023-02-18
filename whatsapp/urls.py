1f0c87051f5548ec9ac03692e3341c276815c94b
Will <happyeungin@gmail.com>
2023-02-18 12:08:07 +0800

    6th change

--- whatsapp/urls.py
+++ whatsapp/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('123', views.whatsAppWebhook, name="whatsapp-webhook"),
+    # path('123', views.whatsAppWebhook, name="whatsapp-webhook"),
 ]
