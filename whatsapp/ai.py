90e3e16db2e688d262810c57f4e1c5ce7fc86f44
Will <happyeungin@gmail.com>
2023-03-03 16:43:05 +0800

    update openai model

--- whatsapp/ai.py
+++ whatsapp/ai.py
@@ -5,18 +5,32 @@ openai.api_key = settings.OPENAI_API_KEY
 
 def startPrompt(instruction):
 
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        # prompt="Reply in the context language(maximum 200 characters)# Context:{} #".format(instruction),
-        prompt=instruction + "\n",
-        temperature=0.7,
-        max_tokens=512,
-        n=1
+    # response = openai.Completion.create(
+    #     model="text-davinci-003",
+    #     # prompt="Reply in the context language(maximum 200 characters)# Context:{} #".format(instruction),
+    #     prompt=instruction + "\n",
+    #     temperature=0.7,
+    #     max_tokens=512,
+    #     n=1
+    # )
+
+    response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    message=[
+        {
+            "role": "user", 
+            "content":instruction,
+        }
+        ],
+    temperature=0.7,
+    max_tokens=512,
+    n=1
     )
+
 
     if 'choices' in response:
         if len(response['choices'])>=0:
-            answer = response['choices'][0]['text']
+            answer = response['choices'][0]['message']['content']
             # answer.replace('\n', '<br/>')
             return answer
         else:
